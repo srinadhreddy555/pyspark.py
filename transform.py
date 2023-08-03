3ec2b8b4c8da019dae1fe242a4c2240cd1b66a07
srinadhreddy555 <135858311+srinadhreddy555@users.noreply.github.com>
2023-08-03 15:59:20 -0400

    Add files via upload

---
+++ transform.py
@@ -0,0 +1,43 @@
+
+# Imports
+from pyspark.sql import SparkSession
+
+# Create SparkSession
+spark = SparkSession.builder \
+            .appName('SparkByExamples.com') \
+            .getOrCreate()
+
+# Prepare Data
+simpleData = (("Java",4000,5), \
+    ("Python", 4600,10),  \
+    ("Scala", 4100,15),   \
+    ("Scala", 4500,15),   \
+    ("PHP", 3000,20),  \
+  )
+columns= ["CourseName", "fee", "discount"]
+
+# Create DataFrame
+df = spark.createDataFrame(data = simpleData, schema = columns)
+df.printSchema()
+df.show(truncate=False)
+
+# Custom transformation 1
+from pyspark.sql.functions import upper
+def to_upper_str_columns(df):
+    return df.withColumn("CourseName",upper(df.CourseName))
+
+# Custom transformation 2
+def reduce_price(df,reduceBy):
+    return df.withColumn("new_fee",df.fee - reduceBy)
+
+# Custom transformation 3
+def apply_discount(df):
+    return df.withColumn("discounted_fee",  \
+             df.new_fee - (df.new_fee * df.discount) / 100)
+
+# transform() usage
+df2 = df.transform(to_upper_str_columns) \
+        .transform(reduce_price,1000) \
+        .transform(apply_discount) 
+                
+df2.show()
